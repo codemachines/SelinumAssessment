50044268d1c0921c3a76bdfcfc9a098e437a4d41
codemachines <34547854+codemachines@users.noreply.github.com>
2023-07-11 14:39:09 -0400

    Create selinum test case to open ui

--- src/test/java/testNg.java
+++ src/test/java/testNg.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class testNg {
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Epic("UiTest")
+@Feature("getGoogle")
+public class testNg {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        // Set up WebDriver
+        System.setProperty("webdriver.chrome.driver", "E:\\Java\\introduction\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    @Story("testGoogle")
+    @Description("test case to open a ui")
+    public void testGoogleSearch() {
+        // Open google.ca
+        driver.get("https://www.google.ca");
+        // Add additional test steps if needed
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // Close the browser
+        driver.quit();
+    }
 }
